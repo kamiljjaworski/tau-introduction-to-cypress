5b16b03b5bf3cdad26e56bfc9863338c12c035e1
Kamil Jaworski <Kamil.Jaworski@GAINCapital.com>
2021-08-17 21:26:28 +0200

    [chapter-03] Interacting with elements, setting timeout

--- cypress/integration/openApp.spec.ts
+++ cypress/integration/openApp.spec.ts
@@ -3,3 +3,8 @@
 it('open app', () => {
     cy.visit('/');
 })
+
+it('interact with elements', () => {
+    cy.visit('/');
+    cy.get('.new-todo', { timeout: 4000 }).type('Learn Cypress' + '{enter}')
+})
