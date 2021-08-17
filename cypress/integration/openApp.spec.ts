f9c273a52986710139a6a3bf358108cf9ad8e447
Kamil Jaworski <Kamil.Jaworski@GAINCapital.com>
2021-08-17 21:33:55 +0200

    [chapter-04] Validations: have text, is checked, has descendants, has css attribute

--- cypress/integration/openApp.spec.ts
+++ cypress/integration/openApp.spec.ts
@@ -8,3 +8,15 @@ it('interact with elements', () => {
     cy.visit('/');
     cy.get('.new-todo', { timeout: 4000 }).type('Learn Cypress' + '{enter}')
 })
+
+it('add item to todo list', () => {
+    const item: string = 'Learn Cypress';
+    cy.visit('/');
+    cy.get('.new-todo').type(item + '{enter}');
+    cy.get('label').should('have.text', item);
+    cy.get('.toggle').should('not.be.checked');
+    cy.get('.toggle').click();
+    cy.get('label').should('have.css', 'text-decoration-line', 'line-through')
+    cy.contains('Clear').click();
+    cy.get('.todo-list').should('not.have.descendants', 'li');
+})
